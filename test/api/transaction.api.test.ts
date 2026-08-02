e35f292d219136bb3e59711aa69a5681a32ac366
wahid.anwar <anwar.wahid1686@gmail.com>
2026-08-02 19:59:53 +0600

    Added chainid to create wallet in transaction api test

--- test/api/transaction.api.test.ts
+++ test/api/transaction.api.test.ts
@@ -19,6 +19,7 @@ describe('Transaction API', () => {
         const receiver = await createWallet({
             tenantId: user.tenantId,
             ownerId: user.id,
+            chainId: 31337,
             address: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
         });
 
@@ -196,6 +197,7 @@ describe('Transaction API', () => {
         const receiver = await createWallet({
             tenantId: user.tenantId,
             ownerId: user.id,
+            chainId: 31337,
             address: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
         });
         const payload = {
