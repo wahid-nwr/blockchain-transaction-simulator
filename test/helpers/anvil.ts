7c90e1742b7faa28783e5708d0b43e5e98093d3e
wahid.anwar <anwar.wahid1686@gmail.com>
2026-08-09 17:27:16 +0600

    Updated type to constants

--- test/helpers/anvil.ts
+++ test/helpers/anvil.ts
@@ -1,12 +1,13 @@
 import { createTestClient, http, parseEther, type Address } from 'viem';
 import { localhost } from 'viem/chains';
+import type { Hex } from 'viem';
 
 export const ANVIL_ACCOUNTS = {
-    deployer: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+    deployer: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80' as Hex,
 
-    user: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+    user: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d' as Hex,
 
-    receiver: '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
+    receiver: '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a' as Hex,
 };
 
 const testClient = createTestClient({
